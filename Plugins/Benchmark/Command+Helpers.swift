e04b589c72d878610d28e3d99f19632ea59b1d32
Joakim Hassila <jocke@ordo.one>
2023-03-14 13:25:51 +0100

    feat: Add support for percentiles TSV output

--- Plugins/Benchmark/Command+Helpers.swift
+++ Plugins/Benchmark/Command+Helpers.swift
@@ -22,17 +22,26 @@ enum Command: String, CaseIterable {
 
 /// The benchmark data output format.
 enum OutputFormat: String, CaseIterable {
-    /// Text output formatted into a visual table
+    /// Text output formatted into a visual table suitable for console output
     case text
-    /// The text output format, formatted in markdown
+    /// The text output format, formatted in markdown, suitable for GitHub workflows 
     case markdown
-    /// Influx data import
+    /// Influx data format
     case influx
-    case percentiles
-    case tsv
-    case jmh
+    case jmh // JMH format consumable by http://jmh.morethan.io
     /// The encoded representation of the underlying histograms capturing the benchmark data.
-    case encodedHistogram
+//    case percentiles // --> histogramPercentilesTXT
+//    case tsv // --> histogramSamplesTSV
+//    case encodedHistogram // --> histogramEncodedJSON
+    /// The encoded representation of the underlying histograms capturing the benchmark data, for programmatic use (Codable).
+    case histogramEncoded
+    /// The histogram percentiles, average, deviation, sample count etc in standard HDR Histogram text format consumable by http://hdrhistogram.github.io/HdrHistogram/plotFiles.html
+    case histogram
+    /// The raw histogram samples in TSV format for processing by external tools (e.g. Youplot)
+    case histogramSamples
+    /// The percentiles values betwen (0-99, 99.9, 99.99, ... 99.99999, 100) in TSV format for processing by external tools (e.g. Youplot)
+    case histogramPercentiles
+
 }
 
 enum Grouping: String, CaseIterable {
--- Plugins/BenchmarkTool/Command+Helpers.swift
+++ Plugins/BenchmarkTool/Command+Helpers.swift
@@ -11,9 +11,9 @@
 // This file need to be manually copied between the Benchmark plugin and
 // the BenchmarkTool when updated, as no external dependencies are allowed
 // for SwiftPM command tools. The source file is in Benchmark and should be
-// edited there, then manually copies to BenchmarkTool after edited.
+// edited there, then manually copies to BenchmarkTool AND BenchmarkHelpGenerator after edited.
 
-enum Command: String {
+enum Command: String, CaseIterable {
     case run
     case list
     case baseline
@@ -22,20 +22,29 @@ enum Command: String {
 
 /// The benchmark data output format.
 enum OutputFormat: String, CaseIterable {
-    /// Text output formatted into a visual table
+    /// Text output formatted into a visual table suitable for console output
     case text
-    /// The text output format, formatted in markdown
+    /// The text output format, formatted in markdown, suitable for GitHub workflows
     case markdown
-    /// Influx data import
+    /// Influx data format
     case influx
-    case percentiles
-    case tsv
-    case jmh
+    case jmh // JMH format consumable by http://jmh.morethan.io
     /// The encoded representation of the underlying histograms capturing the benchmark data.
-    case encodedHistogram
+    //    case percentiles // --> histogramPercentilesTXT
+    //    case tsv // --> histogramSamplesTSV
+    //    case encodedHistogram // --> histogramEncodedJSON
+    /// The encoded representation of the underlying histograms capturing the benchmark data, for programmatic use (Codable).
+    case histogramEncoded
+    /// The histogram percentiles, average, deviation, sample count etc in standard HDR Histogram text format consumable by http://hdrhistogram.github.io/HdrHistogram/plotFiles.html
+    case histogram
+    /// The raw histogram samples in TSV format for processing by external tools (e.g. Youplot)
+    case histogramSamples
+    /// The percentiles values betwen (0-99, 99.9, 99.99, ... 99.99999, 100) in TSV format for processing by external tools (e.g. Youplot)
+    case histogramPercentiles
+
 }
 
-enum Grouping: String {
+enum Grouping: String, CaseIterable {
     case metric
     case benchmark
 }
