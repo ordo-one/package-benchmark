98b3f0dbdf84d1c4c98d5c11e95ea27e93518258
Joakim Hassila <jocke@ordo.one>
2023-03-24 17:58:51 +0100

    Apply PR feedback

--- Sources/SwiftRuntimeHooks/include/SwiftRuntimeHooks.h
+++ Sources/SwiftRuntimeHooks/include/SwiftRuntimeHooks.h
@@ -1,7 +1,5 @@
-#ifndef SwiftRuntimeHooks_hpp
-#define SwiftRuntimeHooks_hpp
-
-#include <stdio.h>
+#ifndef PACKAGE_BENCHMARK_SWIFT_RUNTIME_HOOKS_H
+#define PACKAGE_BENCHMARK_SWIFT_RUNTIME_HOOKS_H
 
 typedef void (*swift_runtime_hook_t)(const void *, void *);
 
--- Sources/SwiftRuntimeHooks/shims.c
+++ Sources/SwiftRuntimeHooks/shims.c
@@ -1,6 +1,5 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <stdint.h>
+#include <stddef.h>
 
 #include "SwiftRuntimeHooks.h"
 
