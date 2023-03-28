100daf7a810121d1229939c0e4014d48300eb758
Joakim Hassila <jocke@ordo.one>
2023-03-28 16:16:03 +0200

    Adding support for benchmark init command.

--- Plugins/BenchmarkCommandPlugin/BenchmarkPlugin+Help.swift
+++ Plugins/BenchmarkCommandPlugin/BenchmarkPlugin+Help.swift
@@ -12,7 +12,10 @@ let help =
     """
     OVERVIEW: Run benchmarks or update, compare or check performance baselines
 
-    Performs operations on benchmarks (running or listing them), as well as storing, comparing baselines as well as checking them for threshold deviations.
+    Performs operations on benchmarks (running or listing them), as well as storing, comparing baselines as well as checking them for threshold
+    deviations.
+
+    The init command will create a skeleton benchmark suite for you and add it to Package.swift.
 
     For the 'text' default format, the output is implicitly 'stdout' unless otherwise specified.
     For all other formats, the output is to a file in either the current working directory, or
@@ -25,6 +28,7 @@ let help =
     USAGE: swift package benchmark <command>
 
        swift package benchmark [run] <options>
+       swift package benchmark init <benchmarkTargetName>
        swift package benchmark list
        swift package benchmark baseline list
        swift package benchmark baseline read <baseline> [<baseline2> ... <baselineN>] [<options>]
@@ -35,7 +39,8 @@ let help =
        swift package benchmark help
 
     ARGUMENTS:
-    <command>               The benchmark command to perform, one of: ["run", "list", "baseline", "help"]. If not specified, 'run' is implied.
+    <command>               The benchmark command to perform, one of: ["run", "list", "baseline", "help", "init"]. If not specified, 'run' is
+                          implied.
 
     OPTIONS:
     --filter <filter>       Benchmarks matching the regexp filter that should be run
@@ -43,11 +48,14 @@ let help =
     --target <target>       Benchmark targets matching the regexp filter that should be run
     --skip-target <skip-target>
                           Benchmark targets matching the regexp filter that should be skipped
-    --format <format>       The output format to use, one of: ["text", "markdown", "influx", "jmh", "histogramEncoded", "histogram", "histogramSamples", "histogramPercentiles"], default is 'text'
-    --metric <metric>       Specifies that the benchmark run should use one or more specific metrics instead of the ones defined by the benchmarks, valid values are: ["cpuUser", "cpuSystem", "cpuTotal", "wallClock",
-                          "throughput", "peakMemoryResident", "peakMemoryVirtual", "mallocCountSmall", "mallocCountLarge", "mallocCountTotal", "allocatedResidentMemory", "memoryLeaked", "syscalls",
-                          "contextSwitches", "threads", "threadsRunning", "readSyscalls", "writeSyscalls", "readBytesLogical", "writeBytesLogical", "readBytesPhysical", "writeBytesPhysical", "retainCount",
-                          "releaseCount", "retainReleaseDelta", "custom"]
+    --format <format>       The output format to use, one of: ["text", "markdown", "influx", "jmh", "histogramEncoded", "histogram",
+                          "histogramSamples", "histogramPercentiles"], default is 'text'
+    --metric <metric>       Specifies that the benchmark run should use one or more specific metrics instead of the ones defined by the
+                          benchmarks, valid values are: ["cpuUser", "cpuSystem", "cpuTotal", "wallClock", "throughput", "peakMemoryResident",
+                          "peakMemoryVirtual", "mallocCountSmall", "mallocCountLarge", "mallocCountTotal", "allocatedResidentMemory",
+                          "memoryLeaked", "syscalls", "contextSwitches", "threads", "threadsRunning", "readSyscalls", "writeSyscalls",
+                          "readBytesLogical", "writeBytesLogical", "readBytesPhysical", "writeBytesPhysical", "retainCount", "releaseCount",
+                          "retainReleaseDelta", "custom"]
     --path <path>           The path where exported data is stored, default is the current directory (".").
     --quiet                 Specifies that output should be suppressed (useful for if you just want to check return code)
     --scale                 Specifies that some of the text output should be scaled using the scalingFactor (denoted by '*' in output)
