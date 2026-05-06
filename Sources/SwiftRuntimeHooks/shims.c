64116da04d4396dd84dca31f88558ea931e6b5dc
Mirza Ucanbarlic <56406159+supersonicbyte@users.noreply.github.com>
2026-05-06 18:38:13 +0200

    user correct abi

--- Sources/SwiftRuntimeHooks/shims.c
+++ Sources/SwiftRuntimeHooks/shims.c
@@ -17,13 +17,13 @@ extern HeapObject * (*_swift_allocObject)(HeapMetadata const *metadata,
                                           size_t requiredAlignmentMask);
 
 extern HeapObject * (*_swift_retain)(HeapObject*);
-extern HeapObject * (*_swift_release)(HeapObject*);
+extern void (*_swift_release)(HeapObject*);
 
 extern HeapObject * (*_swift_tryRetain)(HeapObject*);
 
 // unclear if the following two needs hooking and they don't seem to be called on Apple Silicon
 extern HeapObject * (*_swift_retain_n)(HeapObject *object, uint32_t n);
-extern HeapObject * (*_swift_release_n)(HeapObject *object, uint32_t n);
+extern void (*_swift_release_n)(HeapObject *object, uint32_t n);
 
 #if defined(__linux__)
 extern _Atomic bool _swift_enableSwizzlingOfAllocationAndRefCountingFunctions_forInstrumentsOnly;
@@ -33,6 +33,13 @@ struct hook_data_s {
     HeapObject * (*orig)(HeapObject*);
     HeapObject * (*origTry)(HeapObject*);
     HeapObject * (*orig_n)(HeapObject*, uint32_t);
+    swift_runtime_hook_t hook;
+    void * context;
+};
+
+struct hook_data_release_s {
+    void (*orig)(HeapObject*);
+    void (*orig_n)(HeapObject*, uint32_t);
     swift_runtime_hook_t hook;
     void * context;
 };
@@ -164,44 +171,42 @@ void swift_runtime_set_retain_hook(swift_runtime_hook_t hook, void * context) {
 
 /*===========================================================================*/
 
-static struct hook_data_s _swift_release_hook_data = {NULL, NULL, NULL, NULL, NULL};
+static struct hook_data_release_s _swift_release_hook_data = {NULL, NULL, NULL, NULL};
 
-static HeapObject * _swift_release_hook(HeapObject * heapObject) {
+static void _swift_release_hook(HeapObject * heapObject) {
 #if defined(__linux__) && defined(__x86_64__)
     if (!_swift_pointer_looks_mapped(heapObject)) {
-        return heapObject;
+        return;
     }
 #endif
 
-    HeapObject * ret = (*_swift_release_hook_data.orig)(heapObject);
+    (*_swift_release_hook_data.orig)(heapObject);
     (*_swift_release_hook_data.hook)(heapObject, _swift_release_hook_data.context);
-    return ret;
 }
 
 // This doesn't seem to be called for Apple Silicon at least, but keeping it here
-static HeapObject * _swift_release_n_hook(HeapObject * heapObject, uint32_t n) {
+static void _swift_release_n_hook(HeapObject * heapObject, uint32_t n) {
     int i;
 #if defined(__linux__) && defined(__x86_64__)
     if (!_swift_pointer_looks_mapped(heapObject)) {
-        return heapObject;
+        return;
     }
 #endif
 
-    HeapObject * ret = (*_swift_release_hook_data.orig_n)(heapObject, n);
+    (*_swift_release_hook_data.orig_n)(heapObject, n);
     for (i = 0; i < n; i++) {
         (*_swift_release_hook_data.hook)(heapObject, _swift_release_hook_data.context);
     }
-    return ret;
 }
 
 void swift_runtime_set_release_hook(swift_runtime_hook_t hook, void * context) {
     if (hook == NULL) {
         _swift_release = _swift_release_hook_data.orig;
         _swift_release_n = _swift_release_hook_data.orig_n;
-        struct hook_data_s hook_data = {NULL, NULL, NULL, NULL, NULL};
+        struct hook_data_release_s hook_data = {NULL, NULL, NULL, NULL};
         _swift_release_hook_data = hook_data;
     } else {
-        struct hook_data_s hook_data = {_swift_release, NULL, _swift_release_n, hook, context};
+        struct hook_data_release_s hook_data = {_swift_release, _swift_release_n, hook, context};
         _swift_release_hook_data = hook_data;
         _swift_release = _swift_release_hook;
         _swift_release_n = _swift_release_n_hook;
